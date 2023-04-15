554436e0088cfb72bbc2eb387e907b7dfcd7f7cb
Dwima Basumatary <dwima1583@gmail.com>
2023-04-15 18:53:26 +0530

    try

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const publicDir = path.join(__dirname, 'src/public')
 const apiKey = process.env.UNSPLASH_API_KEY;
 
 let count = 20;
-let totalApiRequests = 30;
+let totalApiRequests = 5;
 
 app.use(express.static(publicDir));
 
